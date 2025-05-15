8f1c0d5a03e4d17c91fc3e72fa121c7a37fb9275
David Nicolalde <nicolaldeda@gmail.com>
2025-05-15 02:48:05 -0700

    web scrapping first commit

--- tutorial_yt.py
+++ tutorial_yt.py
@@ -1,1 +1,26 @@
-print("hello world")
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+
+web='https://www.audible.com/search'
+service = Service(executable_path = 'D:\\\\Users\\\\dnicolalde\\\\Downloads\\\\chromedriver-win64\\\\chromedriver.exe')
+# Set up the Chrome WebDriver
+driver = webdriver.Chrome(service=service)
+# Set the URL of the page you want to scrape
+driver.get(web)
+# Wait for the page to load
+products = driver.find_elements(by='xpath', value ='//li[contains(@class, "productListItem")]')   
+
+for product in products:
+    # Extract the title, author, and runtime
+    title = product.find_element(by='xpath', value ='.//h3[contains(@class, "bc-heading")]').text
+    author = product.find_element(by='xpath', value ='.//li[contains(@class, "authorLabel")]').text
+    runtime = product.find_element(by='xpath', value ='.//li[contains(@class, "runtimeLabel")]').text
+
+    # Print the extracted information
+    print(f'Title: {title}')
+    print(f'Author: {author}')
+    print(f'Runtime: {runtime}')
+    print('---')
+
+driver.quit()
